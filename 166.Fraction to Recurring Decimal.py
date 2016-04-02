57318716a8aa336d30948d13f94dd742821cab47
LandyGuo <gqp.hust@gmail.com>
2016-04-02 21:19:00 +0800

    Update 166.Fraction to Recurring Decimal.py

--- 166.Fraction to Recurring Decimal.py
+++ 166.Fraction to Recurring Decimal.py
@@ -1,6 +1,6 @@
 '''
-166. Fraction to Recurring Decimal My Submissions QuestionEditorial Solution
-Total Accepted: 28692 Total Submissions: 192194 Difficulty: Medium
+166. Fraction to Recurring Decimal 
+Difficulty: Medium
 Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
 
 If the fractional part is repeating, enclose the repeating part in parentheses.
